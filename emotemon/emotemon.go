f2c48b8a4aeecfd8b26a3eed67dfff9a70a16b7c
tyzeller <tyzeller@cisco.com>
2016-07-24 12:50:56 -0700

    Update emotemon.go, compiles and comments added.

--- emotemon/emotemon.go
+++ emotemon/emotemon.go
@@ -1,37 +1,64 @@
 package emotemon
 
 import (
-    	import "math/rand"
+    	"math/rand"
+    	"time"
 )
 
 type Emotemon struct {
         emotemon string
         cp int
-        started bool
-        bot Bot
+        CaptureCount int
 }
 
-func New(bot *Bot) *Emotemon {
-    return &Emotemon {
+/*
+* Generates random values for an emotemon
+* Ensures that CaptureCount == CP and CP > 0
+*/
+func (emotemon *Emotemon) generateEmotemon() {
+
+	// static array of several emotes.
+	//TODO: get emotes from Twitch.tv API
+	emotes := [5]string{
+		"Kappa",
+		"BibleThump",
+		"BabyRage",
+		"KappaPride",
+		"BrainSlug",
+	}
+
+	// Creating a seed for the RNG
+	source := rand.NewSource(time.Now().UnixNano())
+	// The RNG
+    rng := rand.New(source)
+
+    // Randomly generating the capture value for this emotemon (ensure the value is not 0)
+    capture := rng.Intn(100) + 1
+
+    // Set CP
+    emotemon.cp = capture
+
+    // Set capture count initially to the CP
+    emotemon.CaptureCount = capture
+
+    // Set the emotemon
+    emotemon.emotemon = emotes[rng.Intn(5)]
+}
+
+/*
+* Creates a new emotemon
+*/
+func New() *Emotemon {
+
+	emotemon := &Emotemon {
                 emotemon: "Kappa",
                 cp: 1,
-                started: false,
-                bot: *bot,
+                CaptureCount: 1,
         }
+
+    emotemon.generateEmotemon()
+
+    return emotemon
 }
 
-func ReceiveLine(user string, line string) {
-
-}
-
-func (emotemon *Emotemon) Start() {
-	emotemon.generateEmotemon()
-	
-}
-
-func (emotemon *Emotemon) generateEmotemon() {
-	source := rand.NewSource(time.Now().UnixNano())
-    rng := rand.New(source)
-    emotemon.cp = rng.Intn(100)
-}
 
